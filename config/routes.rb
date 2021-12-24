04c92bad2edab38a0d215ca2b04200bc98e54abd
dev-swd <devhdswd3a@gmail.com>
2021-12-24 15:50:06 +0900

    閲覧ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/new' => 'posts#new'
-  post 'posts' => 'posts#create'
-  get 'top' => 'homes#top'
+    resources :posts
+    get 'top' => 'homes#top'
 end
